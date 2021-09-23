cc1635d6ff040d335f6cce8b10be9f9b2af16cd0
Tekkiller <paulnjoroge789016@gmail.com>
2021-09-23 16:16:42 +0300

    DI

--- app/src/main/java/com/paul/cryptocurrency/common/Constants.kt
+++ app/src/main/java/com/paul/cryptocurrency/common/Constants.kt
@@ -1,4 +1,6 @@
 package com.paul.cryptocurrency.common
 
 object Constants {
+
+    const val BASE_URL = "https://api.coiaprika.com./"
 }
---
+++ app/src/main/java/com/paul/cryptocurrency/di/AppModule.kt
@@ -0,0 +1,36 @@
+package com.paul.cryptocurrency.di
+
+import com.paul.cryptocurrency.common.Constants.BASE_URL
+import com.paul.cryptocurrency.data.remote.CoinPaprikaApi
+import com.paul.cryptocurrency.data.repository.CoinRepositoryImpl
+import com.paul.cryptocurrency.domain.repository.CoinRepository
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
+
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AppModule {
+
+
+    @Provides
+    @Singleton
+    fun providePaprikaApi(): CoinPaprikaApi{
+        return Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+            .create(CoinPaprikaApi::class.java)
+    }
+
+    @Provides
+    @Singleton
+    fun provideCoinRepository(api: CoinPaprikaApi): CoinRepository{
+        return CoinRepositoryImpl(api)
+    }
+}
